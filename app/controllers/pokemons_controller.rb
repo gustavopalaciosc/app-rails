0934bcb4d7d9ea00f348e92c6f81ed078907b47f
gustavopalaciosc <gpalaciosc@uc.cl>
2023-04-11 10:10:49 -0400

    Cambios varios pokemons_controller.rb

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,9 @@
+require 'json'
+
 class PokemonsController < ApplicationController
     def index
-        
+        @response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/snorlax")
+        @json_ans = JSON.parse(@response.body)
+        puts @json_ans['abilities']
     end
 end
